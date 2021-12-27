b13cf359d321bb6ff78668196f72a42cfba35a68
eeeyyeeezz <eximmlich@gmail.com>
2021-12-27 17:43:22 +0300

    yes

--- RandomUserGenerator/ViewController.swift
+++ RandomUserGenerator/ViewController.swift
@@ -85,10 +85,7 @@ class ViewController: UIViewController {
 				self.setConstraints()
 			}
 		}
-		
     }
-    
-    
 }
 
 extension ViewController {
@@ -108,55 +105,7 @@ extension ViewController {
 		self.ageLabel.text = "Age: " + String(self.interface.age!)
 		self.addressLabel.text = self.interface.address
 		self.phoneNumberLabel.text = self.interface.phoneNumber
-	}
-	
-    func setConstraints(){
-        self.view.addSubview(self.buttonRandom)
-        NSLayoutConstraint.activate([
-            buttonRandom.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -50),
-            buttonRandom.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            buttonRandom.widthAnchor.constraint(equalToConstant: 300),
-            buttonRandom.heightAnchor.constraint(equalToConstant: 50)
-        ])
-		
-		self.view.addSubview(imageUser)
-		NSLayoutConstraint.activate([
-			imageUser.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
-			imageUser.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-			imageUser.widthAnchor.constraint(equalToConstant: 300),
-			imageUser.heightAnchor.constraint(equalToConstant: 250)
-		])
-		
-		self.view.addSubview(nameLabel)
-		nameLabel.topAnchor.constraint(equalTo: self.imageUser.bottomAnchor, constant: 20).isActive = true
-		setLabelConstraints(nameLabel)
-		
-		self.view.addSubview(emailLabel)
-		emailLabel.topAnchor.constraint(equalTo: self.nameLabel.bottomAnchor, constant: 20).isActive = true
-		setLabelConstraints(emailLabel)
-		
-		self.view.addSubview(ageLabel)
-		ageLabel.topAnchor.constraint(equalTo: self.emailLabel.bottomAnchor, constant: 20).isActive = true
-		setLabelConstraints(ageLabel)
-
-		self.view.addSubview(addressLabel)
-		addressLabel.topAnchor.constraint(equalTo: self.ageLabel.bottomAnchor, constant: 20).isActive = true
-		setLabelConstraints(addressLabel)
-
-		self.view.addSubview(phoneNumberLabel)
-		phoneNumberLabel.topAnchor.constraint(equalTo: self.addressLabel.bottomAnchor, constant: 20).isActive = true
-		setLabelConstraints(phoneNumberLabel)
-        
-    }
-
-	func			setLabelConstraints<Label: UILabel> (_ someLabel: Label){
-		NSLayoutConstraint.activate([
-			someLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-			someLabel.widthAnchor.constraint(equalToConstant: 350),
-			someLabel.heightAnchor.constraint(equalToConstant: 50)
-		])
-	}
-	
+	}	
 	
 	@objc func buttonTapped(){
 		self.userObject.getData()
@@ -168,12 +117,6 @@ extension ViewController {
 			self.interface.phoneNumber = user.phoneNumber
 			self.interface.image = user.image
 		}
-//		print(self.interface.name)
-//		print(self.interface.address)
-//		print(self.interface.email)
-//		print(self.interface.age)
-//		print(self.interface.phoneNumber)
-//		print(self.interface.image)
 		setInterface()
 	}
 	
